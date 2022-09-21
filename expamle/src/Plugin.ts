1a2bfd4e5eaa042ba3eac80522ff999021bcb25b
Water.Li <lizongbin@hotmail.com>
2022-09-21 17:35:32 +0800

    backup

---
+++ expamle/src/Plugin.ts
@@ -0,0 +1,17 @@
+import { Device, IPlugin } from "@appx/plugin-sdk";
+import { mobileComponents, pcComponents, webSiteComponents } from "./components";
+import { locales } from "./loacales";
+
+export const rxPlugin: IPlugin = {
+  id: "predefined.layouts",
+  title: "Layouts",
+  version: "1.0",
+  description: "Description",
+  components: {
+    [Device.PC]: pcComponents,
+    [Device.Mobile]: mobileComponents,
+    [Device.Website]: webSiteComponents,
+  },
+  model: undefined,
+  locales
+}
--- expamle/src/index.ts
+++ expamle/src/index.ts
@@ -1,1 +1,9 @@
-export * from "./model";
+import { IPlugin } from "@appx/plugin-sdk";
+import { rxPlugin } from "./Plugin";
+
+declare const window: Window & { rxPlugin: IPlugin };
+
+(function () {
+  window.rxPlugin = rxPlugin
+  console.log("Layouts plugin 中")
+})()
