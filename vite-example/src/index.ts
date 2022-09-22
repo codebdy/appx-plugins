61e649547bd59ad9b94c1f9030db73e3620154dc
Water.Li <lizongbin@hotmail.com>
2022-09-22 19:14:23 +0800

    backup

--- vite-example/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { IPlugin } from "@rxdrag/appx-plugin-sdk";
-import { rxPlugin } from "./Plugin";
-
-declare const window: Window & { rxPlugin: IPlugin };
-
-(function () {
-  window.rxPlugin = rxPlugin
-  console.log("Example plugin 中")
-})()
